48512d02303ae3c583c95d6f063161651a8de304
Mustafa <startmk2020@gmail.com>
2021-01-21 15:29:44 +0100

    test

--- src/component/Section_1.js
+++ src/component/Section_1.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Header extends Component {
+export default class Section_1 extends Component {
     render() {
         return (
             <div>
-                <h1>Mon header</h1>
-                <h1>test</h1>
+                Ma section 1
             </div>
         )
     }
