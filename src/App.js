f2282b30797933c9d7b4934dd49139d64c0806fe
Mustafa <startmk2020@gmail.com>
2021-01-23 23:13:08 +0100

    modif App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import './App.css';
-import Section_1 from "./component/Section_1";
 import React, { Component } from 'react';
-
-// import { Button } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css"
-
-
+import Header from './component/Header';
+import Carousel1 from './component/Carousel';
+import Formulaire from './component/Formulaire';
+import Section_1 from "./component/Section_1";
 class App extends Component {
-
-	stat = {
-	}
 
 	render() {
 		return (
 			<div className="App" >
-
+				<Header />
+				<Carousel1 />
 				<Section_1 />
-				
-
-
-			
+				<Formulaire />
 			</div>
 		)
 	}
