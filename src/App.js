e0065539be853897ca9bff0e6249231c10466ade
Mustafa <startmk2020@gmail.com>
2021-01-23 23:17:18 +0100

    modif eslintcache

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Header from './component/Header';
 import Carousel1 from './component/Carousel';
 import Formulaire from './component/Formulaire';
-import Section_1 from "./component/Section_1";
 class App extends Component {
 
 	render() {
@@ -12,7 +11,6 @@ class App extends Component {
 			<div className="App" >
 				<Header />
 				<Carousel1 />
-				<Section_1 />
 				<Formulaire />
 			</div>
 		)
